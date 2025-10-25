326dd3b61a77b0d264d78589c993b56be3cd0623
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2025-10-25 01:31:35 +0100

    Async in Translation API

--- app/routers/translation_router.py
+++ app/routers/translation_router.py
@@ -8,6 +8,9 @@ router = APIRouter()
 @router.post("/translate", response_model=TranslationResponse)
 async def translate_and_speak(req: TranslationRequest):
     #language hard-coded to Spanish for test/demo purposes
-    translated_word = translate_word(req.word)
+    translated_word = await translate_word(req.word)
     pronunciation_audio_base64 = synthesize_speech(translated_word)
-    return {"translated_word": translated_word, "pronunciation_audio_base64": pronunciation_audio_base64}
+    return {
+        "translated_word": translated_word,
+        "pronunciation_audio_base64": pronunciation_audio_base64
+        }
