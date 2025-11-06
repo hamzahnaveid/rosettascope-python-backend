e0a71acf0a50a3fa27e8342aaffa1237d087cc71
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2025-11-06 06:17:33 +0000

    Minor refactors

--- app/routers/translation_router.py
+++ app/routers/translation_router.py
@@ -8,7 +8,7 @@ router = APIRouter()
 @router.post("/translate", response_model=TranslationResponse)
 async def translate_and_speak(req: TranslationRequest):
     #language hard-coded to Spanish for test/demo purposes
-    translated_word = await translate_word(req.word)
+    translated_word = await translate_word(req.word, req.targetLanguage)
     pronunciation_audio_base64 = synthesize_speech(translated_word)
     return {
         "translated_word": translated_word,
--- app/services/googletrans_service.py
+++ app/services/googletrans_service.py
@@ -2,7 +2,7 @@ import asyncio
 from googletrans import Translator
 
 #language hard-coded to Spanish for test/demo purposes
-async def translate_word(word: str, target_language: str = "es"):
+async def translate_word(word: str, target_language: str):
     async with Translator() as translator:
         result = await translator.translate(word, dest=target_language)
         return result.text
