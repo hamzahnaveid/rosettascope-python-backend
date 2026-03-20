a11a55b99016c31b6944d1b0960f33104ac7bb14
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2026-03-20 06:59:24 +0000

    BKT Challenge Endpoint

--- app/routers/challenge_router.py
+++ app/routers/challenge_router.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Query
 from app.services.ollama_service import generate_challenge_hints
+from app.services.bkt_service import calculate_challenge_confidence_mastered
 
 router = APIRouter()
 
@@ -8,3 +9,15 @@ router = APIRouter()
 async def get_hints(words: Annotated[list[str] | None, Query()] = None):
     hints = generate_challenge_hints(words)
     return hints
+
+@router.get("/update-bkt-score-challenge/{confidence_mastered}/{isCorrect}", response_model=float)
+async def get_confidence_mastered(confidence_mastered: str, isCorrect: str):
+    confidence_mastered = float(confidence_mastered)
+
+    if (isCorrect == 'true'):
+        isCorrect = True 
+    else : 
+        isCorrect = False
+
+    new_confidence_mastered = calculate_challenge_confidence_mastered(confidence_mastered, isCorrect)
+    return new_confidence_mastered
--- app/services/bkt_service.py
+++ app/services/bkt_service.py
@@ -12,3 +12,18 @@ def calculate_confidence_mastered(confidence_mastered: float, isCorrect: bool):
 
     pLn = ( pL + ((1-pL)*pT) )    
     return pLn
+
+def calculate_challenge_confidence_mastered(confidence_mastered: float, isCorrect: bool):
+    pK = confidence_mastered
+    pT = 0.01
+    pS = 0.3
+    pG = 0.55
+    pL: float
+    if (isCorrect):
+        pL = (pK*(1-pS)) / ( (pK*(1-pS)) + ((1-pK)*(pG)) )
+
+    else:
+        pL = (pK*(pS)) / ( (pK*(pS)) + ((1-pK)*(1-pG)) )
+
+    pLn = ( pL + ((1-pL)*pT) )    
+    return pLn
